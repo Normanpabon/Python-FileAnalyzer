e57aba64177e48c3eb4363cb92a363b651a72e8a
alvarobertin <50723409+alvarobertin@users.noreply.github.com>
2019-05-20 21:50:15 -0500

    Added Incinerate and Incinerator.
    Added: - TurnOnIncinerator: To start the procces to delete all duplicate files
    - Incinerate: To delete a file with the path
    - OutPutResults: Now return True or False if there are duplicates files.

--- Python Duplicate file finder.py
+++ Python Duplicate file finder.py
@@ -49,18 +49,19 @@ def uniteDict(dictA, dictB):
 
 
 def outPutResults(dictA):
-    results = list(filter(lambda x: len(x) > 1, dictA.values()))
-    if len(results) > 0:
-        print('Duplicates Found:', '\n')
-        print('The following are the same, even if the name is different: ', '\n')
-        print('**********************')
-        for result in results:
-            for subresult in result:
-                print('\t\t%s' % subresult)
-            print('**********************')
- 
-    else:
-        print('No duplicate files found.')
+	results = list(filter(lambda x: len(x) > 1, dictA.values()))
+	if len(results) > 0:
+		print('Duplicates Found:', '\n')
+		print('The following are the same, even if the name is different: ', '\n')
+		print('**********************')
+		for result in results:
+			for subresult in result:
+				print('\t\t%s' % subresult)
+			print('**********************')
+		return True
+	else:
+		return False
+	
 
 # Save dictionary on log.txt (file in main folder)
 
@@ -70,24 +71,60 @@ def logArchive(dictio):
 		file.write(json.dumps(dictio))
 
 
+
+def Incinerate(path):
+	"""
+	Recives a path, and erase the archive in that path
+	"""	
+	print("This archive is going to be deleted: ")
+	print(path)
+	print('\n')
+	try:
+		os.remove(path)
+	except OSError as e:
+		print(f'Error: {path} : {e.strerror}')
+
+
+def TurnOnIncinerator(dic):
+	"""
+	Erase everything but the first file of each duplicate in the dictionary
+	"""
+	filesLeft = {}
+	dic = list(dic.values())
+	for Duplicate_files in dic:
+		for file in Duplicate_files[1:]: 
+			#The for starts after the first item duplicated.
+			Incinerate(file)
+
+	print("That's All your Trash...\n")
+
+
+
 # POO Main 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        duplicated = {}
-        folders = sys.argv[1:]
-        for i in folders:
-            # Iterate the folders given
-            if os.path.exists(i):
-                # Find the duplicated files and append them to the dictionary
-                uniteDict(duplicated, duplicatedFiles(i))
-            else:
-                print('%s is not a valid path, please verify' % i)
-                sys.exit()
-        outPutResults(duplicated)
-        print('Log archive in main directory, saved as log.txt. ')
-        logArchive(duplicated)
-    else:
-        print('Usage: python archive.py folder or folders to analyse.')
+	if len(sys.argv) > 1:
+		duplicated = {}
+		folders = sys.argv[1:]
+		for i in folders:
+			# Iterate the folders given
+			if os.path.exists(i):
+			# Find the duplicated files and append them to the dictionary
+				uniteDict(duplicated, duplicatedFiles(i))
+			else:
+				print('%s is not a valid path, please verify' % i)
+				sys.exit()
+		if outPutResults(duplicated):
+			Go = int(input("What you want to do? \n 1) Delete All of the Duplicates \n 2) Exit \n"))
+			if Go == 1:
+				TurnOnIncinerator(duplicated)
+
+				print('Log archive in main directory, saved as log.txt. ')
+				logArchive(duplicated)
+		else:
+			print('No duplicate files found... Byeeeee!')
+
+	else:
+		print('Usage: python archive.py folder or folders to analyse.')
 
 
 
